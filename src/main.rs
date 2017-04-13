565c23d24457f50bde23b0519d560c9c522386ff
Dan Connolly <dckc@madmode.com>
2017-04-13 01:27:23 -0500

    cargo fmt / rustfmt

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ impl GameState {
     // 3. Your module must have a print function that formats the current
     // state of the board as a string.
     pub fn print(&self) -> String {
-        let mut rev_out: Vec<String> = vec!();
+        let mut rev_out: Vec<String> = vec![];
         let sep = BoardConfig::between_columns();
 
         if let Some(which) = self.winner() {
@@ -114,7 +114,7 @@ impl GameState {
         let look = self.players.iter().enumerate().find(|&(_, p)| p.loc == ix);
         let player_spot = match look {
             Some((which, _)) => player_name(which),
-            None => ' '
+            None => ' ',
         };
         format!("{}{}", player_spot, self.board.cells[ix - 1].label())
     }
@@ -123,7 +123,9 @@ impl GameState {
     // 2. The last cell is the winning cell. If a player lands on it, by any
     // means, they win.
     pub fn winner(&self) -> Option<char> {
-        self.players.iter().enumerate()
+        self.players
+            .iter()
+            .enumerate()
             .find(|&(_, p)| p.loc >= self.board.size())
             .map(|(ix, _)| player_name(ix))
     }
@@ -139,24 +141,24 @@ impl GameState {
         match command {
             Board { columns: y, rows: x } => self.board.set_size(y, x),
             Players(n) => self.set_player_count(n),
-            Dice(ds) => { self.dice = ds },
-            Ladder { starts: s, ends: e } =>
-                self.board.set_cell_prop(s, LadderStart { end: e }),
-            Snake { starts: s, ends: e } =>
-                self.board.set_cell_prop(s, SnakeStart { end: e }),
-            PowerUps { typ: pt, cells: cs } =>
+            Dice(ds) => self.dice = ds,
+            Ladder { starts: s, ends: e } => self.board.set_cell_prop(s, LadderStart { end: e }),
+            Snake { starts: s, ends: e } => self.board.set_cell_prop(s, SnakeStart { end: e }),
+            PowerUps { typ: pt, cells: cs } => {
                 for c in cs {
                     self.board.set_cell_prop(c, PowerUp(pt.clone()))
-                },
-            Turns(qty) =>
+                }
+            }
+            Turns(qty) => {
                 for _ in 0..qty {
                     // println!("turn {} of {}", turn + 1, qty);
                     for which_player in 0..self.players.len() {
                         if self.player_turn_wins(which_player) {
-                            return
+                            return;
                         }
                     }
-                },
+                }
+            }
         }
     }
 
@@ -190,7 +192,7 @@ impl GameState {
         };
         if start_loc + delta > self.board.size() {
             // println!("cannot move");
-            return false
+            return false;
         }
 
         let mut current_player = who;
@@ -199,11 +201,15 @@ impl GameState {
             let land_loc = {
                 let player = &mut self.players[current_player];
 
-                if self.board.move_wins(player, delta) { return true }
+                if self.board.move_wins(player, delta) {
+                    return true;
+                }
                 player.loc
             };
 
-            let already = self.players.iter().enumerate()
+            let already = self.players
+                .iter()
+                .enumerate()
                 .find(|&(ix, p)| p.loc == land_loc && ix != current_player);
             if let Some((which, _)) = already {
                 current_player = which;
@@ -234,7 +240,7 @@ struct PlayerState {
     // times by any player, but they do not accumulate - a player
     // either has the powerup or they don't.
     // @@ can a player have one of each at the same time?
-    powerup: Option<PowerType>
+    powerup: Option<PowerType>,
 }
 
 impl PlayerState {
@@ -296,10 +302,12 @@ impl BoardConfig {
     // and forth.
     fn back_and_forth(&self, row: usize, column: usize) -> CellIx {
         // ideally we'd make an iterator, but that's a pain.
-        (row - 1) * self.columns + (
-            if row % 2 == 1 { column }
-            else { self.columns - column + 1 }
-        )
+        (row - 1) * self.columns +
+        (if row % 2 == 1 {
+            column
+        } else {
+            self.columns - column + 1
+        })
     }
 
     // 3. Lines between rows are drawn as sequences
@@ -309,11 +317,13 @@ impl BoardConfig {
     }
 
     // 4. Lines between columns are drawn with | characters, as shown.
-    fn between_columns() -> char { '|' }
+    fn between_columns() -> char {
+        '|'
+    }
 
     // 5. Cells are printed in 2 lines. The first line is the cell
     // number, right justified with spaces.
-    fn cell_first_line(ix: CellIx) -> String{
+    fn cell_first_line(ix: CellIx) -> String {
         format!("{:>3}", ix)
     }
 
@@ -329,13 +339,12 @@ impl BoardConfig {
         let cell_qty = self.cells.len();
         player.loc += delta;
         let cell = &self.cells[player.loc - 1];
-        
+
         let mut win = false;
         match *cell {
-            Plain => {},
+            Plain => {}
             Winning => win = true,
-            SnakeStart {..} if player.powerup == Some(Antivenom) =>
-                player.use_antivenom(),
+            SnakeStart { .. } if player.powerup == Some(Antivenom) => player.use_antivenom(),
             SnakeStart { end: there } => player.go(there),
             LadderStart { end: there } if player.powerup == Some(Escalator) => {
                 player.use_escalator(there);
@@ -369,8 +378,8 @@ impl CellProperty {
         use CellProperty::*;
         match self {
             &Plain => "  ".into(),
-            &SnakeStart{..} => " S".into(),
-            &LadderStart{..} => " L".into(),
+            &SnakeStart { .. } => " S".into(),
+            &LadderStart { .. } => " L".into(),
             &PowerUp(ref pt) => pt.label() + " ",
             &Winning => "  ".into(),
         }
@@ -399,7 +408,7 @@ enum PowerType {
 
     // 3. powerup double 5 sub-command:
     // doubles the next dice roll.
-    Double
+    Double,
 }
 
 impl PowerType {
@@ -410,34 +419,34 @@ impl PowerType {
             "escalator" => Escalator,
             "antivenom" => Antivenom,
             "double" => Double,
-            _ => panic!("bad powerup: {}", name)
+            _ => panic!("bad powerup: {}", name),
         }
     }
 
     fn label(&self) -> String {
         match self {
-            &PowerType::Escalator => "e",
-            &PowerType::Antivenom => "a",
-            &PowerType::Double => "d",
-        }.into()
+                &PowerType::Escalator => "e",
+                &PowerType::Antivenom => "a",
+                &PowerType::Double => "d",
+            }
+            .into()
     }
 }
 
 
 #[derive(PartialEq, Eq, Debug)]
 enum Command {
-
     // 1. board 3 4 command: specifies the number of columns and rows
     // for the board.
     Board { columns: usize, rows: usize },
 
     // 2. players 2 command: specified the number of players, who are
     // named: A, B, ... There can be up to 26 players.
-    Players (PlayerIx),
+    Players(PlayerIx),
 
     // 3. dice 1 2 2 2 2 command: specifies the sequence of dice
     // rolls.
-    Dice (Vec<DieDots>),
+    Dice(Vec<DieDots>),
 
     // 4. ladder 5 11 command: creates a ladder that starts at the
     // first number and ends at the second number
@@ -453,7 +462,7 @@ enum Command {
 
     // 7. turns 10 command: plays the specified number of turns (or
     // until a player wins the game).
-    Turns (usize)
+    Turns(usize),
 }
 
 
@@ -470,53 +479,67 @@ impl Command {
     fn readFrom(line: String) -> Command {
         use Command::*;
 
-        let num8 = |txt: &str| txt
-            .parse::<u8>().expect("bad number");
-        let num = |txt: &str| txt
-            .parse::<CellIx>().expect("bad number");
+        let num8 = |txt: &str| {
+            txt.parse::<u8>()
+                .expect("bad number")
+        };
+        let num = |txt: &str| {
+            txt.parse::<CellIx>()
+                .expect("bad number")
+        };
 
         let mut parameters = line.split(' ');
         let keyword = parameters.next().expect("no keyword");
         match (keyword, parameters.collect::<Vec<_>>()) {
-            ("board", ref ps) if ps.len() == 2 =>
-                Board{ columns: num(ps[0]),
-                       rows: num(ps[1]) },
+            ("board", ref ps) if ps.len() == 2 => {
+                Board {
+                    columns: num(ps[0]),
+                    rows: num(ps[1]),
+                }
+            }
 
-            ("players", ref ps) if ps.len() == 1 =>
-                Players(num8(ps[0]) as PlayerIx),
+            ("players", ref ps) if ps.len() == 1 => Players(num8(ps[0]) as PlayerIx),
 
             ("dice", ps) => Dice(ps.iter().map(|s| num8(*s)).collect()),
 
-            ("ladder", ref ps) if ps.len() == 2 =>
-                Ladder { starts: num(ps[0]),
-                         ends: num(ps[1]) },
+            ("ladder", ref ps) if ps.len() == 2 => {
+                Ladder {
+                    starts: num(ps[0]),
+                    ends: num(ps[1]),
+                }
+            }
 
-            ("snake", ref ps) if ps.len() == 2 =>
-                Snake { starts: num(ps[0]),
-                        ends: num(ps[1]) },
+            ("snake", ref ps) if ps.len() == 2 => {
+                Snake {
+                    starts: num(ps[0]),
+                    ends: num(ps[1]),
+                }
+            }
 
-            ("powerup", ref ps) => PowerUps {
-                typ: PowerType::new(ps[0]),
-                cells: ps[1..].iter().map(|s| num(*s)).collect() },
+            ("powerup", ref ps) => {
+                PowerUps {
+                    typ: PowerType::new(ps[0]),
+                    cells: ps[1..].iter().map(|s| num(*s)).collect(),
+                }
+            }
 
             ("turns", ref ps) if ps.len() == 1 => Turns(num(ps[0])),
 
-            _ => panic!("bad command: {}", line)
+            _ => panic!("bad command: {}", line),
         }
     }
 }
 
 
-/*
-5. Put your ownership information (see the assignment page) in
-the assign4/README.md file.
-
-6. The marker should be able to run your program by entering the following code:
-cargo run
-board 3 4
-players 2
-turns 5
-*/
+// 5. Put your ownership information (see the assignment page) in
+// the assign4/README.md file.
+//
+// 6. The marker should be able to run your program by entering the following code:
+// cargo run
+// board 3 4
+// players 2
+// turns 5
+//
 
 // 7. In addition to any tests or test data we provide, you must have
 // unit tests for the components of your program, verifying that they
@@ -529,7 +552,10 @@ mod test {
     fn command_parse1() {
         use Command::*;
         assert!(Command::readFrom("board 3 4".into()) ==
-                Board { columns: 3, rows: 4});
+                Board {
+            columns: 3,
+            rows: 4,
+        });
     }
 
     #[test]
@@ -539,23 +565,38 @@ mod test {
         let lines: Vec<_> = SAMPLE_INPUT.trim().lines().collect();
 
         assert!(Command::readFrom(lines[0].into()) ==
-                Board { columns: 3, rows: 4});
-        assert!(Command::readFrom(lines[1].into()) ==
-                Players(2));
-        assert!(Command::readFrom(lines[2].into()) ==
-                Dice(vec!(1, 2, 2, 2, 2)));
+                Board {
+            columns: 3,
+            rows: 4,
+        });
+        assert!(Command::readFrom(lines[1].into()) == Players(2));
+        assert!(Command::readFrom(lines[2].into()) == Dice(vec![1, 2, 2, 2, 2]));
         assert!(Command::readFrom(lines[3].into()) ==
-                Ladder{ starts: 5, ends: 11});
+                Ladder {
+            starts: 5,
+            ends: 11,
+        });
         assert!(Command::readFrom(lines[4].into()) ==
-                Snake{ starts: 8, ends: 4});
+                Snake {
+            starts: 8,
+            ends: 4,
+        });
         assert!(Command::readFrom(lines[5].into()) ==
-                PowerUps{ typ: Escalator, cells: vec!(6, 9) });
+                PowerUps {
+            typ: Escalator,
+            cells: vec![6, 9],
+        });
         assert!(Command::readFrom(lines[6].into()) ==
-                PowerUps{ typ: Antivenom, cells: vec!(7) });
+                PowerUps {
+            typ: Antivenom,
+            cells: vec![7],
+        });
         assert!(Command::readFrom(lines[7].into()) ==
-                PowerUps{ typ: Double, cells: vec!(4) });
-        assert!(Command::readFrom(lines[8].into()) ==
-                Turns(10));
+                PowerUps {
+            typ: Double,
+            cells: vec![4],
+        });
+        assert!(Command::readFrom(lines[8].into()) == Turns(10));
 
     }
 
